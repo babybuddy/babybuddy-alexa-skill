2e6ca57d895a8af448c4460137756bcf345e980c
Robbie Plankenhorn <rplankenhorn@gmail.com>
2020-08-10 14:12:38 -0500

    Added launch.json to setup script.

--- bin/setup.sh
+++ bin/setup.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
-ASK_CLI_FILES=( "$SCRIPT_DIR/../.ask/ask-states.json" "$SCRIPT_DIR/../skill-package/skill.json" )
+SETUP_FILES=( "$SCRIPT_DIR/../.ask/ask-states.json" "$SCRIPT_DIR/../skill-package/skill.json" "$SCRIPT_DIR/../.vscode/launch.json" )
 
 for file in ${ASK_CLI_FILES[@]}; do
   if [[ ! -f "$file" ]]; then

91057af67191c0b53dc150ce83aac8617b034fac
Robbie Plankenhorn <rplankenhorn@gmail.com>
2020-08-11 11:18:10 -0500

    Removed dotenv support.

--- bin/setup.sh
+++ bin/setup.sh
@@ -9,18 +9,3 @@ for file in ${ASK_CLI_FILES[@]}; do
     cp $file.example $file
   fi
 done
-
-DOT_ENV_FILE="$SCRIPT_DIR/../lambda/custom/.env"
-
-if [[ ! -f $DOT_ENV_FILE ]]; then
-  echo -n "Enter Baby Buddy server URL (e.g. https://babybuddy.url.com/): "
-
-  read BABY_BUDDY_API_URL
-
-  echo -n "Enter Baby Buddy API key: "
-
-  read BABY_BUDDY_API_KEY
-
-  echo "BABY_BUDDY_API_URL=$BABY_BUDDY_API_URL" >> $DOT_ENV_FILE
-  echo "BABY_BUDDY_API_KEY=$BABY_BUDDY_API_KEY" >> $DOT_ENV_FILE
-fi

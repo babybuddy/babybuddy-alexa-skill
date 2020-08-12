44a5a0eee50c575700c01d06d94b322e1bd3de4f
Robbie Plankenhorn <rplankenhorn@gmail.com>
2020-08-12 12:21:44 -0500

    Fixed typo in setup.sh.

--- bin/setup.sh
+++ bin/setup.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 SETUP_FILES=( "$SCRIPT_DIR/../.ask/ask-states.json" "$SCRIPT_DIR/../skill-package/skill.json" "$SCRIPT_DIR/../.vscode/launch.json" )
 
-for file in ${ASK_CLI_FILES[@]}; do
+for file in ${SETUP_FILES[@]}; do
   if [[ ! -f "$file" ]]; then
     cp $file.example $file
   fi

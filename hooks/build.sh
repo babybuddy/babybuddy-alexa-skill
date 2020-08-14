25b5ebd960cbd222d509b86ea6d00236ce5350d2
Robbie Plankenhorn <rplankenhorn@gmail.com>
2020-08-14 12:19:07 -0500

    Added logic for doing a custom build when running ask deploy.

--- hooks/build.sh
+++ hooks/build.sh
@@ -4,5 +4,4 @@ SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 cd $SCRIPT_DIR/../lambda/custom
 yarn build
-
-zip -r $SCRIPT_DIR/../lambda/custom/build.zip $SCRIPT_DIR/../lambda/custom/.build > /dev/null 2>&1
+yarn zip
